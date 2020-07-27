cb4b7610f25348ee767d59a7d5a5bee27f1d87be
student <anu.troy@gmail.com>
2020-07-27 10:13:04 +0100

    DEMO- cf push manifest, test

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,19 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    String val;
+
+    public WelcomeController(@Value("${welcome.message}") String value){
+        this.val=value;
+    }
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return val;
     }
 }
